a8efef83d02fc19ee93791824d7f600a1cac67cb
A1509 <erazoalexis16@gmail.com>
2022-08-15 14:04:48 -0500

    hola como esta

--- Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/js/reserva.js
+++ Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/js/reserva.js
@@ -20,6 +20,9 @@ var close = document.getElementById("btn-cerrar-popup");
 var boton = document.getElementById("boton"); 
 var noma = document.getElementById("name").value;
 var cedula = document.getElementById("cedula").value;
+var hora = document.getElementById("hora").value;
+var hora2 = document.getElementById("hora2").value;
+var fech = document.getElementById("fechaReserva").value;
 /* js de como funciona el overlay */
 boton.addEventListener("click" ,function(){
 overlay.classList.add("active") ;
@@ -32,11 +35,17 @@ pop.classList.remove("active");
 })
 
 function reservacion(){
-if(cedula == null && name ==null ){
- alert("nel")
-} else{
-alert("FELICIDADES, SU RESERVACION HA SIDO EXITOSA")
-}
+var nombres, cedula;
+  var nombres = document.getElementById("name").value;
+    var cedula = document.getElementById("cedula").value;
 
+    if (nombres == "" && cedula == "") {
 
-}
+          console.log("correcto");
+          alert("LO SIENTO, SU RESERVACION NO SE HA COMPLETADO");
+              
+        } else {
+          console.log("error");
+          alert("FELICIDADES " + nombres + " EL LUGAR DE SU RESERVACION TIENE SU CEDULA: " + cedula + " LA CUAL USTED DEBE ENTREGAR EL DIA QUE RESERVO " );
+        }
+      }
