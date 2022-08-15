692e6e27f6754e5783b6846ad22228d71a349c52
josueskt <77248411+josueskt@users.noreply.github.com>
2022-08-15 15:02:28 -0500

    1

--- Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/interfaz/reg-in/app/stratic/css/img/js/registro.js
+++ Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/interfaz/reg-in/app/stratic/css/img/js/registro.js
@@ -1,67 +1,90 @@
-function agregarUsuario(){
- var Nombre= document.getElementById("name").value;
- var Apellido= document.getElementById("apellido").value;
- var Contraseña= document.getElementById("contraseña").value;
- var Cedula=document.getElementById("cedula").value;
- var Correo= document.getElementById("correo").value;
- var Sexo= document.querySelector('input[name="sexo"]:checked').value;
- var Direccion= document.getElementById("direccion").value;
- var Nacimiento= document.querySelector('input[name="nacimiento"]').value;
- var Telefono= document.getElementById("telefono").value;
- 
- datos= [];
+var datos = [];
+  
+
+function agregarUsuario() {
+  var Nombre = document.getElementById("name").value;
+  var Apellido = document.getElementById("apellido").value;
+ var Contraseña = document.getElementById("contraseña").value;
+  var Cedula = document.getElementById("cedula").value;
+ var Correo = document.getElementById("correo").value;
+  var Sexo = document.querySelector('input[name="sexo"]:checked').value;
+  var Direccion = document.getElementById("direccion").value;
+  var Nacimiento = document.querySelector('input[name="nacimiento"]').value;
+  var Telefono = document.getElementById("telefono").value;
 
 
-if(Cedula == null){
+  if (Cedula == null) {
     alert("el numero de cedula es obligatoria");
     document.write("cedula").focus();
-}
-if(Nombre == null){
+  }
+  if (Nombre == null) {
     alert("el nombre del usuario es obligatorio");
     document.write("name").focus();
-}
-if(Contraseña == null){
+  }
+  if (Contraseña == null) {
     alert("la contraseña es obligatoria");
     document.write("contraseña").focus();
-}else{
-    datos.push(Contraseña)
-    
-}
-if(Nacimiento == null){
+  } else {
+    datos.push(Contraseña);
+  }
+  if (Nacimiento == null) {
     alert("la fecha de nacimiento es obligatoria");
     document.write("nacimiento").focus();
-}
-if(Direccion == null){
+  }
+  if (Direccion == null) {
     alert("la direccion es obligatoria");
     document.write("direccion").focus();
-}
-if(Sexo == null){
+  }
+  if (Sexo == null) {
     alert("establezca su sexo es obligatorio");
     document.write("sexo").focus();
-}
-if(Correo== null){
+  }
+  if (Correo == null) {
     alert("el correo es obligatoria");
     document.write("correo").focus();
-}else{
-    datos.push(Correo)
-   
-    
-}
+  } else {
+    datos.push(Correo);
+  }
 
-if(Telefono== null){
+  if (Telefono == null) {
     alert("el telefono es obligatoria");
     document.write("telefono").focus();
-}
-if(Apellido== null){
+  }
+  if (Apellido == null) {
     alert("el apellido es obligatorio");
     document.write("apellido").focus();
+  }
+  window.location.href = "../../../interfaz/reg-in/web/login.html";
+  //window.open("../../../../Outstanding parks of Ecuador.html");
+  alert(datos);
 }
-window.open("../../../../Outstanding parks of Ecuador.html");
-alert(datos)
+
+function login() {
+  
+  var usuariu = document.getElementById("correu").value; 
+  
+  var cotra = document.getElementById("contra").value; 
+ 
+ 
+  
+  alert(datos) ;
+  if (usuariu == 1234 && cotra == 1234) {
+   
+    window.location.href = "../../../../Outstanding parks of Ecuador.html";
+    //window.open("../../../../Outstanding.html");
+    
+  }
+ 
+    if (usuariu == usuario && cotra == clave) {
+      alert("hola")
+      window.location.href = "../../../../Outstanding parks of Ecuador.html";
+      //window.open("../../../../Outstanding.html");
+      
+    }
+    else{
+      alert("tmr")
+    }
+  
+
 
 }
-
-
-
-
-console.log(Nombre + "" + Apellido + "" + Contraseña + "" + Cedula + "" + Correo + "" + Sexo + Nacimiento + Direccion + "" + Telefono+"" + Idioma)
