e6aca9f53ce8a394d7f9541ced6c2415dcc17eb0
josueskt <77248411+josueskt@users.noreply.github.com>
2022-08-16 21:34:04 -0500

    .l.l

--- Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/js/reserva.js
+++ Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/js/reserva.js
@@ -1,28 +1,12 @@
-function procesar(){
-    var comment= document.getElementById("prueba").value
-    alert(comment);
-    
-    document.getElementById("todosComentario").innerText <= comment;
-    }
 
      // Obtener fecha actual
-let fecha = new Date();
-// Agregar 3 días
-fecha.setDate(fecha.getDate() + 2);
-// Obtener cadena en formato yyyy-mm-dd, eliminando zona y hora
-let fechaMin = fecha.toISOString().split('T')[0];
-// Asignar valor mínimo
-document.querySelector('#fechaReserva').min = fechaMin; 
+
 
 var overlay = document.getElementById("overlay")
 var pop = document.getElementById("popup")
 var close = document.getElementById("btn-cerrar-popup"); 
 var boton = document.getElementById("boton"); 
-var noma = document.getElementById("name").value;
-var cedula = document.getElementById("cedula").value;
-var hora = document.getElementById("hora").value;
-var hora2 = document.getElementById("hora2").value;
-var fech = document.getElementById("fechaReserva").value;
+
 // parte de reservaciones 
 /* js de como funciona el overlay */
 boton.addEventListener("click" ,function(){
@@ -36,17 +20,36 @@ pop.classList.remove("active");
 })
 
 function reservacion(){
-var nombres, cedula;
+
   var nombres = document.getElementById("name").value;
-    var cedula = document.getElementById("cedula").value;
+  var cedul = document.getElementById("cedula").value;
+ // var h = document.getElementById("hora").value;
+   
+  var fecha = document.getElementById("fechaReserva").value
 
-    if (nombres == "" && cedula == "") {
+  /*  let fecha = new Date();
+// Agregar 3 días
+fecha.setDate(fecha.getDate() + 2);
+// Obtener cadena en formato yyyy-mm-dd, eliminando zona y hora
+let fechaMin = fecha.toISOString().split('T')[0];
+// Asignar valor mínimo
+document.querySelector('#fechaReserva').min = fechaMin; */
 
-          console.log("correcto");
-          alert("LO SIENTO, SU RESERVACION NO SE HA COMPLETADO");
-              
-        } else {
-          console.log("error");
-          alert("FELICIDADES " + nombres + " EL LUGAR DE SU RESERVACION TIENE SU CEDULA: " + cedula + " LA CUAL USTED DEBE ENTREGAR EL DIA QUE RESERVO " );
-        }
+
+    
+
+    if (nombres == "" && cedul=="") 
+    {  
+     
+            console.log("correcto");
+            alert("LO SIENTO, SU RESERVACION NO SE HA COMPLETADO"); 
+     
+    }
+      else {
+            document.getElementById("textito").innerHTML = "FELICIDADES <br>" + nombres + " EL LUGAR DE SU RESERVACION TIENE SU CEDULA: <br>" + cedul + "<br> LA CUAL USTED DEBE ENTREGAR EL DIA QUE RESERVO "  + "  <br>  "+fecha;
+            console.log("error");
+           // alert("FELICIDADES " + nombres + " EL LUGAR DE SU RESERVACION TIENE SU CEDULA: " + cedula + " LA CUAL USTED DEBE ENTREGAR EL DIA QUE RESERVO " );
+
+    }
+    
       }
