265160470c220f125fb76d759537ea0218a88190
josueskt <77248411+josueskt@users.noreply.github.com>
2022-09-12 07:31:11 -0500

    1

--- Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/interfaz/reg-in/app/stratic/css/img/js/registro.js
+++ Outstanding parks of Ecuadorimgtest/Outstanding parks of Ecuador/interfaz/reg-in/app/stratic/css/img/js/registro.js
@@ -1,3 +1,66 @@
+<<<<<<< HEAD
+=======
+var datos = [];
+var Correo =""
+var Contraseña=""
+function agregarUsuario() {
+  var Nombre = document.getElementById("name").value;
+  var Apellido = document.getElementById("apellido").value;
+  Contraseña = document.getElementById("contraseña").value;
+  var Cedula = document.getElementById("cedula").value;
+  Correo = document.getElementById("correo").value;
+  var Sexo = document.querySelector('input[name="sexo"]:checked').value;
+  var Direccion = document.getElementById("direccion").value;
+  var Nacimiento = document.querySelector('input[name="nacimiento"]').value;
+  var Telefono = document.getElementById("telefono").value;
+
+
+  if (Cedula == null) {
+    alert("el numero de cedula es obligatoria");
+    document.write("cedula").focus();
+  }
+  if (Nombre == null) {
+    alert("el nombre del usuario es obligatorio");
+    document.write("name").focus();
+  }
+  if (Contraseña == null) {
+    alert("la contraseña es obligatoria");
+    document.write("contraseña").focus();
+  } else {
+    datos.push(Contraseña);
+  }
+  if (Nacimiento == null) {
+    alert("la fecha de nacimiento es obligatoria");
+    document.write("nacimiento").focus();
+  }
+  if (Direccion == null) {
+    alert("la direccion es obligatoria");
+    document.write("direccion").focus();
+  }
+  if (Sexo == null) {
+    alert("establezca su sexo es obligatorio");
+    document.write("sexo").focus();
+  }
+  if (Correo == null) {
+    alert("el correo es obligatoria");
+    document.write("correo").focus();
+  } else {
+    datos.push(Correo);
+  }
+
+  if (Telefono == null) {
+    alert("el telefono es obligatoria");
+    document.write("telefono").focus();
+  }
+  if (Apellido == null) {
+    alert("el apellido es obligatorio");
+    document.write("apellido").focus();
+  }
+ // window.location.href = "../../../interfaz/reg-in/web/login.html";
+  //window.open("../../../../Outstanding parks of Ecuador.html");
+  alert(datos);
+}
+>>>>>>> parent of dcf9865 (listo)
 
 //login
 function login() {
@@ -15,15 +78,27 @@ function login() {
   
     
   }
+<<<<<<< HEAD
 
   else
   {
+=======
+ 
+   /* if (usuariu == Correo && cotra == Contraseña) {
+      alert("hola")
+      window.location.href = "../../../../Outstanding parks of Ecuador.html";
+      //window.open("../../../../Outstanding.html");
+      
+    }*/
+    else{
+>>>>>>> parent of dcf9865 (listo)
       alert("datos incorrectos")
   }
   
 
 
 }
+<<<<<<< HEAD
 //registro
 function guardar(){
    //elementos de registro
@@ -46,3 +121,5 @@ function guardar(){
   document.getElementById("tablita").appendChild(btn);
 }
 
+=======
+>>>>>>> parent of dcf9865 (listo)
